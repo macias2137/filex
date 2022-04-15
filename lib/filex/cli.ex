d2fc0aa3ab9251ad40ac13a73926c11745d560da
Maciej Moroz <maciek@Maciejs-MacBook-Pro-2.local>
2022-04-15 14:42:22 +0200

    add delete_from_file function

--- lib/filex/cli.ex
+++ lib/filex/cli.ex
@@ -7,20 +7,34 @@ defmodule Filex.CLI do
   end
 
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, strict: [read: :boolean, write: :boolean], aliases: [r: :read, w: :write])
+    parse = OptionParser.parse(argv,
+    strict: [read: :boolean, write: :boolean, delete: :boolean],
+    aliases: [r: :read, w: :write, d: :delete])
 
     case parse do
-     {[read: true], _, _} -> read_file()
+     {[read: true], _, _} -> read_file("File contents")
      {[write: true], user_input, _} -> write_to_file(user_input)
+     {[delete: true], user_input, _} -> delete_from_file(user_input)
     end
   end
 
-  def read_file do
+  def read_file(label) do
     {:ok, contents} = File.read(@text_file_path)
-    IO.inspect(contents, label: "File contents")
+    IO.inspect(contents, label: label)
   end
 
   def write_to_file(input) do
     File.write(@text_file_path, "\n#{input}", [:append])
+  end
+
+  def delete_from_file(input) do
+    new_contents =
+    File.stream!(@text_file_path)
+    |> Stream.map(&String.split(&1, input, trim: true))
+    |> Enum.to_list()
+    |> List.flatten()
+    |> List.to_string()
+    File.write(@text_file_path, new_contents)
+    read_file("New file contents")
   end
 end
